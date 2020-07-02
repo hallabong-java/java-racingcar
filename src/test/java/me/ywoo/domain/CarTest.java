33fb45e40e045ffd11c978a74799e107dfdf0a87
dusdn1702 <dusdn1702@naver.com>
2020-07-02 12:18:49 +0900

    refactor : carTest 수정
    구현 코드 변경에 따른 test 코드 수정

--- src/test/java/me/ywoo/domain/CarTest.java
+++ src/test/java/me/ywoo/domain/CarTest.java
@@ -1,5 +1,5 @@
 /*
- * CarTest.java            1.2       2020-06-27
+ * CarTest.java            2.0       2020-07-02
  *
  * Copyright (c) 2020 Yeonwoo Cho
  * ComputerScience, ProgrammingLanguage, Java, Seoul, KOREA
@@ -18,15 +18,15 @@ import static org.assertj.core.api.Assertions.*;
  * Car 객체 잘 받아오는지 테스트
  *
  * @author 조연우
- * @version 1.1     2020년 6월 27일
+ * @version 2.0     2020년 7월 2일
  */
 
 class CarTest {
 
     @DisplayName("Car(names)_이름 받아서 객체 생성")
     @Test
-    void Car_GenerateInstance(String names) {
-        assertThat(new Car("dusdn", 1)).isInstanceOf(Car.class);
+    void Car_GenerateInstance() {
+        assertThat(new Car("test", 1)).isInstanceOf(Car.class);
     }
 
     @DisplayName("movePosition()_숫자가 4보다 작으면 위치를 그대로 유지")
@@ -45,22 +45,11 @@ class CarTest {
         assertThat(car.getPosition()).isEqualTo(2);
     }
 
-    @DisplayName("compareTwoPosition()_숫자 빼서 반환")
-    @Test
-    public void return_ThisMinusAnother(){
-        final Car testCar1 = new Car("test1", 2);
-        final Car testCar2 = new Car("test2", 4);
-        int actual = testCar1.compareTwoPositions(testCar2);
-        int expected = 2;
-        assertThat(actual).isEqualTo(expected);
-    }
-
     @DisplayName("isWinner()_position이 같은 객체면 true 반환")
     @Test
     public void return_True_SamePosition(){
         final Car testCar1 = new Car("test1", 4);
-        final Car testCar2 = new Car("test2", 4);
-        boolean actual = testCar1.isWinnerToo(testCar2);
+        boolean actual = testCar1.isWinnerToo(4);
         assertThat(actual).isEqualTo(true);
     }
 

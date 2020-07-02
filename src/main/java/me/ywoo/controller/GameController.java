413546a6fadcffdbdd2c713ac4682f44096248cb
dusdn1702 <dusdn1702@naver.com>
2020-07-02 17:04:44 +0900

    refactor : GameController 객체 수정
    불필요한 필드변수 선언을 줄임
    접근 지정자를 올바르게 선정

--- src/main/java/me/ywoo/controller/GameController.java
+++ src/main/java/me/ywoo/controller/GameController.java
@@ -1,5 +1,5 @@
 /*
- * GameController.java            1.0       2020-06-23
+ * GameController.java            2.0       2020-07-02
  *
  * Copyright (c) 2020 Yeonwoo Cho
  * ComputerScience, ProgrammingLanguage, Java, Seoul, KOREA
@@ -22,36 +22,30 @@ import java.util.List;
  * 게임을 진행하는 클래스
  *
  * @author 조연우
- * @version 1.0     2020년 6월 23일
+ * @version 2.0     2020년 7월 2일
  */
 
 public class GameController {
     private static final int MINIMUM_TO_GAME = 0;
-
-    private CarName carName;
-    private Game game;
-    private InputView inputView = new InputView();
-    private OutputView outputView = new OutputView();
-
-    public List<String> winners = new ArrayList<String>();
+    private List<String> winners = new ArrayList<String>();
     private int playCounts;
 
     public void run() {
-        carName = new CarName(inputView.receiveNames());
-        String inputPlayCounts = inputView.receiveNumber();
+        CarName carName = new CarName(InputView.receiveNames());
+        String inputPlayCounts = InputView.receiveNumber();
         validCount(inputPlayCounts);
-        game = new Game(carName);
-        gameStart();
+        Game game = new Game(carName);
+        gameStart(game);
     }
 
-    public void validCount(String inputPlayCount) {
+    private void validCount(String inputPlayCount) {
         playCounts = Integer.parseInt(inputPlayCount);
         if (playCounts <= MINIMUM_TO_GAME) {
             throw new IllegalArgumentException("1회 이상 게임해야 합니다.");
         }
     }
 
-    public void gameStart() {
+    private void gameStart(Game game) {
         OutputView.printResult();
         while (playCounts > MINIMUM_TO_GAME) {
             game.moveAllCars();

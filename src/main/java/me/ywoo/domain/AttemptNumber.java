29af270ca6d58d0d46d04a0f7392ddc1a7fa7a82
dusdn1702 <dusdn1702@naver.com>
2020-07-06 18:30:07 +0900

    refactor : AttemptNumber 객체 생성
    시도 횟수 관리하는 AttemptNumber 생성

---
+++ src/main/java/me/ywoo/domain/AttemptNumber.java
@@ -0,0 +1,26 @@
+package me.ywoo.domain;
+
+public class AttemptNumber {
+    private static final int LOWER_BOUND_OF_ATTEMPT = 1;
+    private static int countsOfPlays;
+
+    public AttemptNumber(final int countsOfPlays) {
+        validateNumber(countsOfPlays);
+        this.countsOfPlays = countsOfPlays;
+    }
+
+    private void validateNumber(int number) {
+        if(number<LOWER_BOUND_OF_ATTEMPT){
+            throw new IllegalArgumentException("1회 이상 게임해야 합니다");
+        }
+    }
+
+    public static int validateCount(String inputPlayCount) {
+        try {
+            countsOfPlays = Integer.parseInt(inputPlayCount);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("횟수는 숫자를 입력해야 합니다.");
+        }
+        return countsOfPlays;
+    }
+}
---
+++ src/test/java/me/ywoo/domain/AttemptNumberTest.java
@@ -0,0 +1,24 @@
+package me.ywoo.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class AttemptNumberTest {
+    @DisplayName("validateNumber() - 유효하지 않은 횟수 예외처리")
+    @Test
+    void validateNumber_invalidCounts_exceptionThrown(){
+        assertThatThrownBy(()->new AttemptNumber(-3))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1회 이상 게임해야 합니다.");
+    }
+
+    @DisplayName("validateCount() - 유효하지 않은 입력 예외처리")
+    @Test
+    void validateCount_invalidInput_exceptionThrown(){
+        assertThatThrownBy(()->AttemptNumber.validateCount("abc"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("횟수는 숫자를 입력해야 합니다.");
+    }
+}
